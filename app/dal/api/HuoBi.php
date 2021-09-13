6afdb2af69ef75fc2502560a5e6ca65c06532705
linyangyang <linyangyang.92@bytedance.com>
2021-09-13 13:41:32 +0800

    账号总额计算,方便统计涨幅

--- app/dal/api/HuoBi.php
+++ app/dal/api/HuoBi.php
@@ -171,7 +171,7 @@ class HuoBi
         $res_str = file_get_contents($url);
         Log::info(sprintf('请求返回: %s', $res_str));
         $res = json_decode($res_str, true);
-        if ('ok' !== $res['status'] && (isset($res['code']) && $res['code'] !== 200)) {
+        if ((isset($res['status']) && 'ok' !== $res['status']) && (isset($res['code']) && $res['code'] !== 200)) {
             throw new APIException($res['err-msg']?? '请求错误' . json_encode($res));
         }
         return $res;
