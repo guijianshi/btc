82861af9db63588fa89b23e7d76b073b72b2ea24
linyangyang <linyangyang.92@bytedance.com>
2021-09-16 22:44:29 +0800

    完成自动买卖脚本重构

--- app/command/btc/OrderAutoTask.php
+++ app/command/btc/OrderAutoTask.php
@@ -4,12 +4,16 @@ declare (strict_types = 1);
 namespace app\command\btc;
 
 use app\dal\api\HuoBi;
+use app\model\Order;
 use think\console\Command;
 use think\console\Input;
 use think\console\Output;
 
 class orderAutoTask extends Command
 {
+    /**
+     * @deprecated
+     */
     protected function configure()
     {
         // 指令配置
@@ -20,8 +24,54 @@ class orderAutoTask extends Command
     protected function execute(Input $input, Output $output)
     {
         // 指令输出
-        $res = (new HuoBi())->kline("btcusdt");
-        $output->writeln('app\command\orderAutoTask');
-        $output->writeln(json_encode($res));
+        $orderDal = new Order();
+        $orders  = $orderDal->getRunningOrders();
+        $huobi = new HuoBi();
+        /* @var Order $order */
+        $subOrderId = microtime(true) * 10000 % 10;
+        foreach ($orders as $order) {
+            // 老订单可以更新频率低一些 三天之前 每次更新十分之一
+            if ($order['mtime'] < date('Y-m-d H:i:s', strtotime('-3 day')) && $order['id'] % 10 != $subOrderId) {
+                continue;
+            }
+            if ($order->status == 0) {
+                $buyResp = $huobi->buy($order->symbol, 'buy-limit', $order->buy_price, $order->num);
+                $order->buy_order_id = $buyResp['order_id'];
+                $order->status = Order::STATUS_BUY_ING;
+                if ($order->save()) {
+                    $this->output->info("买入成功:" . $order->id);
+                } else {
+                    $this->output->error("买入失败:" . $order->id);
+                }
+            } elseif ($order->status == Order::STATUS_BUY_ING) {
+                $queryResp = $huobi->query($order->buy_order_id);
+
+                if ($queryResp['state'] == 'canceled') {
+                    // 撤销订单,将购买任务取消
+                    $order->cancel();
+                } elseif ('filled' !== $queryResp['state']) {
+                    $order->direction = 'sale';
+                    $order->status = Order::STATUS_SALE_WAIT;
+                    if ($order->save()) {
+                        $this->output->info("订单已完成:" . $order->id);
+                    } else {
+                        $this->output->error("订单完成失败:" . $order->id);
+                    }
+                }
+            } elseif ($order->status == Order::STATUS_SALE_WAIT) {
+                $buyResp = $huobi->buy($order->symbol, 'sell-limit', $order->sale_price, $order->num);
+                $order->sale_order_id = $buyResp['order_id'];
+                $order->status = Order::STATUS_SALE_ING;
+                if ($order->save()) {
+                    $this->output->info("创建卖出成功:" . $order->id);
+                } else {
+                    $this->output->error("创建卖出失败:" . $order->id);
+                }
+            } elseif ($order->status == Order::STATUS_SALE_ING) {
+                $queryResp = $huobi->query($order->sale_order_id);
+
+                $order->cancel();
+            }
+        }
     }
 }
--- app/model/Order.php
+++ app/model/Order.php
@@ -25,6 +25,12 @@ use think\Model;
  */
 class Order extends Model
 {
+    const STATUS_WAIT = 0; // 初始化
+    const STATUS_BUY_ING = 1; // 购买中
+    const STATUS_SALE_WAIT = 2; // 待出售
+    const STATUS_SALE_ING = 3; // 出售中
+    const STATUS_CANCEL = 4; // 取消
+
     protected $table = "orders";
 
     public function getRunningOrders($symbol = '')
@@ -33,15 +39,20 @@ class Order extends Model
         if (!empty($symbol)) {
             $query = $query->where('symbol', $symbol);
         }
-        return $query->limit(200)->select();
+        return $query->limit(300)->select();
     }
 
-    public function delOldCancelTask()
+    public function delOldCancelTask():bool
     {
         $query = $this->where('status', 4)
             ->where('grid_id', 0)
             ->where('mtime', '<', date('Y-m-d', strtotime("-1week")));
 
         return $query->limit(20)->delete();
+    }
+
+    public function cancel()
+    {
+        $this->status = 4;
     }
 }
